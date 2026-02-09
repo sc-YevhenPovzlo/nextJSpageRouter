5734a69c520aafe3564d6b3ecf79ff08b56e08d7
Yevhen Povzlo <59564875+sc-YevhenPovzlo@users.noreply.github.com>
2026-02-09 20:12:19 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,10 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest, _response: NextResponse) {
+export function middleware(request: NextRequest) {
   // Custom logic goes here
   console.log('Request URL:', request.url);
 
   // Continue to the next step in the request lifecycle
-  return _response || NextResponse.next();
+  return NextResponse.next();
 }
