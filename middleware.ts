96a944e35e6747852d11888617d85bf4353899f6
Yevhen Povzlo <59564875+sc-YevhenPovzlo@users.noreply.github.com>
2026-02-09 17:28:35 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,11 +6,6 @@ export function middleware(request: NextRequest, _response: NextResponse) {
   // Custom logic goes here
   console.log('Request URL:', request.url);
 
-  // Example: Redirect all requests from the '/about' path to '/home'
-  // if (request.nextUrl.pathname.startsWith('/about')) {
-  //   return NextResponse.redirect(new URL('/page', request.url));
-  // }
-
   // Continue to the next step in the request lifecycle
-  return NextResponse.next();
+  return _response || NextResponse.next();
 }
